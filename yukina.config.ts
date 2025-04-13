d303f8bd6934be1ca66f22401089501846f8fc1a
Luorivergoddess <luorivergoddess@gmail.com>
2025-04-13 05:31:49 -0700

    Refactor code structure for improved readability and maintainability

--- yukina.config.ts
+++ yukina.config.ts
@@ -33,7 +33,7 @@ const YukinaConfig: Configuration = {
 
   username: "Luorivergoddess",
   sign: "手持烟火以谋生\n心怀诗意以谋爱",
-  avatarUrl: "https://s2.loli.net/2025/01/25/FPpTrQSezM8ivbl.webp",
+  avatarUrl: "/images/avatar.webp",
   socialLinks: [
     {
       icon: "line-md:github-loop",
